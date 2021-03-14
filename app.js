7c26fec27ea2ab7d16201469745675c783bd6d8a
sanha <kgpaper@naver.com>
2021-03-14 10:23:09 +0900

    21/03/14 complete

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = "700";
 
 // 캔버스의 크기 지정 (css와 별도)
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 let painting = false;
 let filling = false;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = "INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
 ctx.lineWidth = 2.5;
 
 function stopPainting(event) {
@@ -42,6 +49,7 @@ function onMouseDown(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -59,6 +67,24 @@ function handleModeClick(event) {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick(){
+  const image = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
+}
+
 if (canvas) {
   // mousemove : 마우스 움직임 감지
   canvas.addEventListener("mousemove", onMouseMove);
@@ -68,6 +94,10 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   // mouseleave : 해당 객체에서 마우스가 벗어나는 것을 감지
   canvas.addEventListener("mouseleave", stopPainting);
+
+  canvas.addEventListener("click", handleCanvasClick);
+
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) => color.addEventListener("click", handleColorClick));
@@ -78,4 +108,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+  saveBtn.addEventListener("click", handleSaveClick);
 }
